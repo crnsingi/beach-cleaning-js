be8c192232c3fd076e9b30d6a4f33dac812b513b
Cesar <cesarnsingi@hotmail.com>
2025-01-07 08:53:13 +0100

    js property

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.getElementById('contact-form').addEventListener('submit', function (e) {
     e.preventDefault();
+    
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
