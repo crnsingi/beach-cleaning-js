8b4766abac55090eea83f4c1016621b3d0e3f90e
Cesar <cesarnsingi@hotmail.com>
2025-01-15 17:46:28 +0100

    video playsinline

--- main.js
+++ main.js
@@ -20,3 +20,20 @@ a.appendChild(linkText);
 a.title = "home";
 a.href = "http://beachcleaning.com";
 document.body.appendChild(a);
+
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
