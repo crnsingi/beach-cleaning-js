67903e5f489c49e8c9afd1810c8100b790ce0878
Cesar <cesarnsingi@hotmail.com>
2025-01-23 08:06:11 +0100

    add event listener

--- main.js
+++ main.js
@@ -25,4 +25,8 @@ document.addEventListener('DOMContentLoaded', function() {
   const burger = document.querySelector('.burger-menu');
   const nav = document.querySelector('.nav-links');
 
+  burger.addEventListener('click', function() {
+    
+  })
+
 })
