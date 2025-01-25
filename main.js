02ef492019e40a9a762f8b61a13dabeb8e29b9e2
Cesar <cesarnsingi@hotmail.com>
2025-01-25 10:09:00 +0100

    class list toggle

--- main.js
+++ main.js
@@ -47,5 +47,6 @@ const hamMenu = document.querySelector('.ham-menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
 
 hamMenu.addEventListener('click', () => {
-  
+  hamMenu.classList.toggle('active');
+
 })
