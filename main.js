223df75a0b2cb24bc0d55df1fa1d54b8d9eacdb9
Cesar <cesarnsingi@hotmail.com>
2025-01-25 10:09:34 +0100

    off screen class list

--- main.js
+++ main.js
@@ -48,5 +48,6 @@ const offScreenMenu = document.querySelector('.off-screen-menu');
 
 hamMenu.addEventListener('click', () => {
   hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
 
 })
