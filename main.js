4e6df745ee4cc9fdc69c2f231188ea5b695b77c7
Cesar <cesarnsingi@hotmail.com>
2025-01-25 10:08:28 +0100

    ham menu add event listener

--- main.js
+++ main.js
@@ -45,3 +45,7 @@ document.body.appendChild(a);
 const hamMenu = document.querySelector('.ham-menu');
 
 const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+  
+})
