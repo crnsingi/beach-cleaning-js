c6e0b8d96ebeaf04ac3e426d1090ba83051180ab
Cesar <cesarnsingi@hotmail.com>
2025-03-03 11:57:13 +0200

    import all navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import react from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Home from "./Components/Home";
 import About from "./Components/About";
-import 
+import Portfolio from "./Components/Portfolio";
+import Services from "./Components/Services";
+import Contact from "./Components/Contact";
+
+
 
 const App = () => {
   return (
