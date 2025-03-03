f05123a679a4137fb287c6b06e509734e99fa038
Cesar <cesarnsingi@hotmail.com>
2025-03-03 17:19:30 +0200

    div class home container

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-
 import React from 'react'
+import Navbar from "./Components/Navbar/Navbar"
 
 const Home = () => {
   return (
     <div className="home-container">
         <Navbar />
+        <div className="home-container">
+
+        </div>
       
     </div>
   )
