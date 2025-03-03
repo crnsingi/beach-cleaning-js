d7f5bcba2a9f556976af7e8ae9c1314e92902b9a
Cesar <cesarnsingi@hotmail.com>
2025-03-03 12:50:46 +0200

    debug navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import react from 'react'
+import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-import Home from "./Components/Home";
-import About from "./Components/About";
-import Portfolio from "./Components/Portfolio";
-import Services from "./Components/Services";
-import Contact from "./Components/Contact";
-
-
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Home />
-      <About />
-      <Portfolio />
-      <Services />
-      <Contact />
     </div>
   )
 }
