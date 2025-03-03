c38f69986b362bc2ebc6b934edcbc4f9dd23c656
Cesar <cesarnsingi@hotmail.com>
2025-03-03 11:32:32 +0200

    about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import About from "./Components/About";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <About />
     </div>
   )
 }
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,27 @@
 import React from "react";
 
-const About = () {
-    return ( 
-        <div id="about" className="about-section-container">
+const About = () => {
+  return (
+    <div id="about" className="about-section-container">
+
+      <div className="about-section-text-container">
+        <p className="primary-subheading">About</p>
+        <h1 className="primary-heading">
+          Clean Environment Is An Important Part Of A Good Life
+        </h1>
+        <p className="primary-text">
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+        <p className="primary-text">
+          Non tincidunt magna non et elit. Dolor turpis molestie dui magnis
+          facilisis at fringilla quam.
+        </p>
+        <div className="about-buttons-container">
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
+
+export default About;
