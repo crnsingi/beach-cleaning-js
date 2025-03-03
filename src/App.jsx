a9caf36b4d12181dfee9cdc0c37a9a9e6d175cd5
Cesar <cesarnsingi@hotmail.com>
2025-03-03 11:55:47 +0200

    app navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import react from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Home from "./Components/Home";
 import About from "./Components/About";
+import 
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Home />
       <About />
+      <Portfolio />
+      <Services />
+      <Contact />
     </div>
   )
 }
